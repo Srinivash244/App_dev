92801b1f17c2eb83c8a073f3aa12e4ee2730b534
Srinivash244 <727723euit244@skcet.ac.in>
2025-08-28 10:22:28 +0530

    backend

---
+++ SpringApp/Alumni/src/test/java/com/example/Alumni/AlumniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Alumni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
